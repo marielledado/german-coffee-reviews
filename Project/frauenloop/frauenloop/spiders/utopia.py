e9ba9167b86ff145185579c063499ac9a4e258e4
Marielle <marielledado@users.noreply.github.com>
2019-11-06 15:42:37 +0100

    minor changes to draft

--- Project/frauenloop/frauenloop/spiders/utopia.py
+++ Project/frauenloop/frauenloop/spiders/utopia.py
@@ -6,7 +6,7 @@ import scrapy
 class UtopiaSpider(scrapy.Spider):
     name = 'utopia'
     allowed_domains = ['utopia.de']
-    start_urls = ['https://utopia.de/produkt/j-j-darboven-cafe-intencion/']
+    url = ['https://utopia.de/produkt/j-j-darboven-cafe-intencion/']
     def parse(self, response):
         self.log("I just visited: " + response.url)
         reviews = response.css('div.commenttext p:nth-child(1)::text').extract()
