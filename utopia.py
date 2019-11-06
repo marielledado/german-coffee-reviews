60d1495501a00ea275e5a7f0cbd4ead93c32bcf0
Marielle <marielledado@users.noreply.github.com>
2019-11-06 22:34:28 +0100

    scraped rating and brand name

--- utopia.py
+++ utopia.py
@@ -10,8 +10,20 @@ class UtopiaSpider(scrapy.Spider):
     def parse(self, response):
         self.log("I just visited: " + response.url)
         reviews = response.css('div.commenttext p:nth-child(1)::text').extract()
-        self.log(reviews)
+
+        # self.log(reviews)
+
         for review in reviews:
-            yield {
+            stars = 0
+            for star in response.css("div.static-rating").extract():
+                stars = star.count('<i class="fa fa-star" aria-hidden="true">')
+            coffee_reviews = {
+            "brand":response.css("h1::text").extract(),
+            "rating": stars,
             "review":review
             }
+            yield coffee_reviews
+        # follow pagination
+        next_page_url =  response.css("ul.pagination > li > a::attr(href)").extract_first()
+        if next_page_url:
+            yield scrapy.Request(url=next_page_url, callback=self.parse)
