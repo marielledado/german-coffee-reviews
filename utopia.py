34aac534126485e935bdf36409990078d78e742e
Marielle <marielledado@users.noreply.github.com>
2019-11-07 09:35:44 +0100

    final spider and complete json file

--- utopia.py
+++ utopia.py
@@ -6,19 +6,28 @@ import scrapy
 class UtopiaSpider(scrapy.Spider):
     name = 'utopia'
     allowed_domains = ['utopia.de']
-    start_urls = ['https://utopia.de/produkt/j-j-darboven-cafe-intencion/']
+    start_urls = ['https://utopia.de/bestenlisten/bio-kaffee-fair-trade-kaffee/']
     def parse(self, response):
-        self.log("I just visited: " + response.url)
+        list_of_urls = response.css("h3.product-listing__headline a::attr(href)").extract()
+        for link in list_of_urls:
+            request = scrapy.Request(link,callback=self.parse)
+            yield request
+
+        #extract brand name
+        brand = response.css("h1::text").extract_first()
+        #extract reviews
         reviews = response.css('div.commenttext p:nth-child(1)::text').extract()
 
         # self.log(reviews)
-
+        
         for review in reviews:
+            # counter for star-ratings based on number of i-class instances
             stars = 0
             for star in response.css("div.static-rating").extract():
                 stars = star.count('<i class="fa fa-star" aria-hidden="true">')
+            # dictionary for json
             coffee_reviews = {
-            "brand":response.css("h1::text").extract(),
+            "brand":brand,
             "rating": stars,
             "review":review
             }
